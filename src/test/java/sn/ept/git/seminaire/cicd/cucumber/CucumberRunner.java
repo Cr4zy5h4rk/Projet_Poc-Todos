6e28e1b1579ce126a0ed8e74d13944c7cefd40f8
killer-pro <cissemouhamadoudiouf@gmail.com>
2024-07-12 12:17:17 +0000

    Modification pour tester le workflow

--- src/test/java/sn/ept/git/seminaire/cicd/cucumber/CucumberRunner.java
+++ src/test/java/sn/ept/git/seminaire/cicd/cucumber/CucumberRunner.java
@@ -15,6 +15,24 @@ import org.junit.platform.suite.api.*;
 public class CucumberRunner {
     public static final String GLUE_PROPERTY = "cucumber.glue";
     public static final String GLUE_PROPERTY_VALUE = "sn.ept.git.seminaire.cicd.cucumber";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static final String NAMING_STRATEGY = "cucumber.junit-platform.naming.strategy";
     public static final String NAMING_STRATEGY_VALUE = "long";
     public static final String PUBLISH_QUIET = "cucumber.publish.quiet";
